21e912f314f71dcb6517168a3bc6b940de8a6e1f
Marzio <36334966+MarsMDK@users.noreply.github.com>
2022-10-10 19:52:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-sys.path.append('../src/')
+# sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# sys.path.append('../src/')
+
+sys.path.insert(0, os.path.abspath("../../src/"))
 
 
 # -- Project information -----------------------------------------------------
