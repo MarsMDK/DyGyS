f7ec568215bf19b4456274f1dea12fb78c82904e
Marzio <36334966+MarsMDK@users.noreply.github.com>
2022-10-10 19:42:11 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# sys.path.append('../src/')
+sys.path.insert(0, '../..')
 sys.path.append('../src/')
 
 
